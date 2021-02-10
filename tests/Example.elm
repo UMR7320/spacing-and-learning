5c7e715160c9521c5d288f55ea1190f54a3a55ff
Acourt <acourt.yoelis@gmail.com>
2021-02-10 15:10:30 +0100

    add some tests for dedupe function

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,10 +1,18 @@
 module Example exposing (..)
 
 import Expect exposing (Expectation)
+import Experiment.Scrabble exposing (dedupe)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    describe "The dedupe function"
+        [ test "doubled letters are deduplicated" <|
+            \_ ->
+                Expect.equal [ ( "a", 1 ), ( "a", 2 ), ( "b", 1 ) ] (dedupe [ "a", "a", "b" ])
+        , test "Given empty list return empty list" <|
+            \_ ->
+                Expect.equal [] (dedupe [])
+        ]
