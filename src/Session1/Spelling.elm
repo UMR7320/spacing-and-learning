c0338d25c274651e07641ddeffb11116bde1b59e
kimlai <hello@kimlaitrinh.me>
2021-12-07 09:54:49 +0100

    [session1] save Spelling data
    - data is saved after each item
    - data is saved in the "Spelling1" column of the "users" table

--- src/Session1/Spelling.elm
+++ src/Session1/Spelling.elm
@@ -8,6 +8,7 @@ import Html.Styled.Attributes exposing (class, disabled)
 import Http
 import Json.Decode as Decode
 import Json.Decode.Pipeline exposing (..)
+import Json.Encode as Encode
 import Logic
 import Ports
 import Random
@@ -15,6 +16,23 @@ import Random.List
 import Session2.CU2 exposing (Step(..))
 import Session3.Synonym exposing (Msg(..))
 import View
+
+
+
+-- MODEL
+
+
+type alias State =
+    { inputUid : String
+    , userAnswer : String
+    , remainingListenings : Int
+    , step : Step
+    }
+
+
+type Step
+    = ListeningFirstTime
+    | Answering
 
 
 type alias Spelling =
@@ -32,27 +50,27 @@ type alias Trial =
     }
 
 
-type Msg
-    = UserClickedNextTrial
-    | UserClickedFeedback
-    | UserClickedRadioButton String
-    | UserClickedStartMainloop
-    | UserClickedSavedData
-    | ServerRespondedWithLastRecords (Result Http.Error (List ()))
-    | UserClickedPlayAudio String
-    | UserClickedStartTraining
-    | AudioEnded { eventType : String, name : String, timestamp : Int }
+iniState : State
+iniState =
+    State "DefaultTrialUID" "" 3 ListeningFirstTime
 
 
-updateWhenNextTrial model shuffleOptionsMsg =
-    ( { model
-        | spellingLvl1 =
-            Logic.next iniState model.spellingLvl1
+start info trials =
+    let
+        id =
+            "recJOpE5pMTCHJOSV"
 
-        -- ICI GROS BUG À VENIR, il faut reset uniquement la réponse du volontaire
-      }
-    , Random.generate shuffleOptionsMsg (Random.List.shuffle model.optionsOrder)
-    )
+        relatedInfos =
+            Dict.get id (ExperimentInfo.toDict info) |> Result.fromMaybe ("I couldn't fetch the value associated with: " ++ id)
+    in
+    Logic.startIntro relatedInfos
+        (List.filter (\datum -> datum.isTraining) trials)
+        (List.filter (\datum -> not datum.isTraining) trials)
+        iniState
+
+
+
+-- VIEW
 
 
 viewInstructions txt =
@@ -90,67 +108,7 @@ viewTask data currentTrial optionsOrder =
     ]
 
 
-update msg model =
-    let
-        taskId =
-            "recJOpE5pMTCHJOSV"
-
-        currentSpellingState =
-            Logic.getState model.spellingLvl1 |> Maybe.withDefault iniState
-    in
-    case msg of
-        UserClickedFeedback ->
-            ( { model
-                | spellingLvl1 =
-                    model.spellingLvl1
-                        |> Logic.toggle
-              }
-            , Cmd.none
-            )
-
-        UserClickedRadioButton newChoice ->
-            ( { model
-                | spellingLvl1 =
-                    Logic.update { currentSpellingState | userAnswer = newChoice } model.spellingLvl1
-              }
-            , Cmd.none
-            )
-
-        UserClickedNextTrial ->
-            ( { model | spellingLvl1 = Logic.next iniState model.spellingLvl1 }, Cmd.none )
-
-        UserClickedStartMainloop ->
-            ( { model | spellingLvl1 = Logic.startMain model.spellingLvl1 iniState }, Cmd.none )
-
-        UserClickedSavedData ->
-            let
-                responseHandler =
-                    ServerRespondedWithLastRecords
-            in
-            ( model, Logic.saveData responseHandler model.user taskId model.spellingLvl1 )
-
-        ServerRespondedWithLastRecords _ ->
-            ( model, Cmd.none )
-
-        UserClickedPlayAudio url ->
-            ( { model | spellingLvl1 = Logic.update { currentSpellingState | remainingListenings = currentSpellingState.remainingListenings - 1 } model.spellingLvl1 }, Ports.playAudio url )
-
-        UserClickedStartTraining ->
-            ( { model | spellingLvl1 = Logic.startTraining model.spellingLvl1 }, Cmd.none )
-
-        AudioEnded { eventType } ->
-            case eventType of
-                "SoundEnded" ->
-                    ( { model | spellingLvl1 = Logic.update { currentSpellingState | step = Answering } model.spellingLvl1 }, Cmd.none )
-
-                _ ->
-                    ( model, Cmd.none )
-
-
-view :
-    Logic.Task Trial State
-    -> List Int
-    -> Html Msg
+view : Logic.Task Trial State -> List Int -> Html Msg
 view exp optionsOrder =
     case exp of
         Logic.Loading ->
@@ -192,20 +150,6 @@ view exp optionsOrder =
                     View.end infos.end UserClickedSavedData "context-understanding"
 
 
-subscriptions model =
-    case model.spellingLvl1 of
-        Logic.Running _ { state } ->
-            case state.step of
-                ListeningFirstTime ->
-                    Sub.batch [ Ports.audioEnded AudioEnded ]
-
-                _ ->
-                    Sub.none
-
-        _ ->
-            Sub.none
-
-
 viewAudioButton nTimes url =
     case nTimes of
         3 ->
@@ -221,17 +165,102 @@ viewAudioButton nTimes url =
             div [] []
 
 
-type alias State =
-    { inputUid : String
-    , userAnswer : String
-    , remainingListenings : Int
-    , step : Step
-    }
+
+-- UPDATE
 
 
-type Step
-    = ListeningFirstTime
-    | Answering
+type Msg
+    = UserClickedNextTrial
+    | UserClickedFeedback
+    | UserClickedRadioButton String
+    | UserClickedStartMainloop
+    | UserClickedSavedData
+    | UserClickedPlayAudio String
+    | UserClickedStartTraining
+    | AudioEnded { eventType : String, name : String, timestamp : Int }
+    | HistoryWasSaved (Result Http.Error String)
+
+
+update msg model =
+    let
+        taskId =
+            "recJOpE5pMTCHJOSV"
+
+        currentSpellingState =
+            Logic.getState model.spellingLvl1 |> Maybe.withDefault iniState
+    in
+    case msg of
+        UserClickedFeedback ->
+            ( { model
+                | spellingLvl1 =
+                    model.spellingLvl1
+                        |> Logic.toggle
+              }
+            , Cmd.none
+            )
+
+        UserClickedRadioButton newChoice ->
+            ( { model
+                | spellingLvl1 =
+                    Logic.update { currentSpellingState | userAnswer = newChoice } model.spellingLvl1
+              }
+            , Cmd.none
+            )
+
+        UserClickedNextTrial ->
+            let
+                newModel =
+                    { model | spellingLvl1 = Logic.next iniState model.spellingLvl1 }
+            in
+            ( newModel
+            , saveData newModel
+            )
+
+        UserClickedStartMainloop ->
+            ( { model | spellingLvl1 = Logic.startMain model.spellingLvl1 iniState }, Cmd.none )
+
+        -- data is now saved after each "trial", so this does nothing and shoud be removed
+        UserClickedSavedData ->
+            ( model, Cmd.none )
+
+        UserClickedPlayAudio url ->
+            ( { model | spellingLvl1 = Logic.update { currentSpellingState | remainingListenings = currentSpellingState.remainingListenings - 1 } model.spellingLvl1 }, Ports.playAudio url )
+
+        UserClickedStartTraining ->
+            ( { model | spellingLvl1 = Logic.startTraining model.spellingLvl1 }, Cmd.none )
+
+        AudioEnded { eventType } ->
+            case eventType of
+                "SoundEnded" ->
+                    ( { model | spellingLvl1 = Logic.update { currentSpellingState | step = Answering } model.spellingLvl1 }, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        HistoryWasSaved _ ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions model =
+    case model.spellingLvl1 of
+        Logic.Running _ { state } ->
+            case state.step of
+                ListeningFirstTime ->
+                    Sub.batch [ Ports.audioEnded AudioEnded ]
+
+                _ ->
+                    Sub.none
+
+        _ ->
+            Sub.none
+
+
+
+-- HTTP
 
 
 decodeTrials : Decode.Decoder (List Trial)
@@ -275,25 +304,59 @@ getRecords =
         }
 
 
-start info trials =
-    let
-        id =
-            "recJOpE5pMTCHJOSV"
-
-        relatedInfos =
-            Dict.get id (ExperimentInfo.toDict info) |> Result.fromMaybe ("I couldn't fetch the value associated with: " ++ id)
-    in
-    Logic.startIntro relatedInfos
-        (List.filter (\datum -> datum.isTraining) trials)
-        (List.filter (\datum -> not datum.isTraining) trials)
-        iniState
-
-
 getTrialsFromServer : (Result Http.Error (List Trial) -> msg) -> Cmd msg
 getTrialsFromServer callbackMsg =
     Data.getTrialsFromServer_ "input" "Meaning" callbackMsg decodeTrials
 
 
-iniState : State
-iniState =
-    State "DefaultTrialUID" "" 3 ListeningFirstTime
+saveData model =
+    let
+        history =
+            Logic.getHistory model.spellingLvl1
+                |> List.filter (\( trial, _ ) -> not trial.isTraining)
+
+        userId =
+            model.user |> Maybe.withDefault "recd18l2IBRQNI05y"
+
+        payload =
+            updateHistoryEncoder userId history
+    in
+    Http.request
+        { method = "PATCH"
+        , headers = []
+        , url = Data.buildQuery { app = Data.apps.spacing, base = "users", view_ = "Session1_output" }
+        , body = Http.jsonBody payload
+        , expect = Http.expectJson HistoryWasSaved (Decode.succeed "OK")
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+updateHistoryEncoder : String -> List ( Trial, State ) -> Encode.Value
+updateHistoryEncoder userId history =
+    -- The Netflify function that receives PATCH requests only works with arrays
+    Encode.list
+        (\_ ->
+            Encode.object
+                [ ( "id", Encode.string userId )
+                , ( "fields", historyEncoder userId history )
+                ]
+        )
+        [ ( userId, history ) ]
+
+
+historyEncoder : String -> List ( Trial, State ) -> Encode.Value
+historyEncoder userId history =
+    Encode.object
+        -- airtable does not support JSON columns, so we save giant JSON strings
+        [ ( "Spelling1", Encode.string (Encode.encode 0 (Encode.list historyItemEncoder history)) )
+        ]
+
+
+historyItemEncoder : ( Trial, State ) -> Encode.Value
+historyItemEncoder ( { uid, target }, { userAnswer } ) =
+    Encode.object
+        [ ( "trialUid", Encode.string uid )
+        , ( "target", Encode.string target )
+        , ( "answser", Encode.string userAnswer )
+        ]
