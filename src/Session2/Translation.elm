2e0bea2126c4bd0da28498fb0c25e4804c09bad0
kimlai <hello@kimlaitrinh.me>
2021-12-07 10:01:56 +0100

    [session2] re-organize translation code

--- src/Session2/Translation.elm
+++ src/Session2/Translation.elm
@@ -22,16 +22,141 @@ import View
 
 
 
---getTrialsFromServer : Decodera -> b
+-- MODEL
 
 
-getTrialsFromServer : (Result Http.Error (List Trial) -> msg) -> Cmd msg
-getTrialsFromServer msgHandler =
-    Data.getTrialsFromServer_ "input" "Translation" msgHandler decodeTrials
+type alias SummarizedTrial =
+    { trialuid : String, userUid : String, attempt : String }
+
+
+type alias Trial =
+    { uid : String
+    , question : String
+    , target : String
+    , translation2 : String
+    , distractor1 : String
+    , distractor2 : String
+    , distractor3 : String
+    , word : String
+    , isTraining : Bool
+    }
+
+
+type alias State =
+    { uid : String
+    , userAnswer : String
+    }
+
+
+defaultTrial : Trial
+defaultTrial =
+    { uid = ""
+    , question = "String"
+    , target = "String"
+    , translation2 = "String"
+    , distractor1 = ""
+    , distractor2 = ""
+    , distractor3 = ""
+    , word = "String"
+    , isTraining = False
+    }
+
+
+initState : State
+initState =
+    { uid = ""
+    , userAnswer = ""
+    }
+
+
+start : List ExperimentInfo.Task -> List Trial -> Logic.Task Trial State
+start info trials =
+    let
+        relatedInfos =
+            Dict.get taskId (ExperimentInfo.toDict info) |> Result.fromMaybe ("I couldn't fetch the value associated with: " ++ taskId)
+    in
+    Logic.startIntro relatedInfos
+        (List.filter (\datum -> datum.isTraining) trials)
+        (List.filter (\datum -> not datum.isTraining) trials)
+        initState
 
 
 
--- Translation
+-- VIEW
+
+
+view : { task : Logic.Task Trial State, optionsOrder : List comparable } -> Html Msg
+view task =
+    case task.task of
+        Logic.Running Logic.Training data ->
+            case data.current of
+                Just trial ->
+                    div [ class "flex flex-col items-center" ]
+                        [ View.trainingWheelsGeneric (List.length data.history) data.infos.trainingWheel [ trial.target ]
+                        , renderTask task trial data data.history data.trainingTrials
+                        ]
+
+                Nothing ->
+                    View.introToMain UserClickedStartMain
+
+        Logic.Running Logic.Main data ->
+            case data.current of
+                Just trial ->
+                    div [ class "flex flex-col items-center" ]
+                        [ renderTask task trial data data.history data.mainTrials
+                        ]
+
+                Nothing ->
+                    View.end data.infos.end UserClickedSaveData "spelling"
+
+        Logic.Loading ->
+            View.loading
+
+        Logic.NotStarted ->
+            div [] [ text "The experiment is not started yet" ]
+
+        Logic.Err reason ->
+            div [] [ text reason ]
+
+        Logic.Running Logic.Instructions data ->
+            div [] [ View.instructions data.infos UserClickedStartTraining ]
+
+
+renderTask task trial data history allTrials =
+    div [ class "w-full" ]
+        [ if List.isEmpty data.trainingTrials then
+            View.tooltip data.infos.instructions_short
+
+          else
+            div [] []
+        , if List.length data.trainingTrials > 0 then
+            div [] []
+
+          else
+            Progressbar.progressBar history allTrials
+        , View.fromMarkdown trial.question
+        , div
+            [ class "w-full pt-8", disabled data.feedback ]
+          <|
+            View.shuffledOptions
+                data.state
+                data.feedback
+                UserClickedRadioButton
+                trial
+                task.optionsOrder
+        , View.genericSingleChoiceFeedback
+            { isVisible = data.feedback
+            , userAnswer = data.state.userAnswer
+            , target = trial.target
+            , feedback_Correct = ( data.infos.feedback_correct, [ trial.target ] )
+            , feedback_Incorrect = ( data.infos.feedback_incorrect, [ trial.target ] )
+            , button = View.navigationButton UserClickedToggleFeedback UserClickedNextTrial data.feedback data.state.userAnswer
+            }
+        ]
+
+
+
+-- UPDATE
 
 
 type Msg
@@ -83,44 +208,29 @@ update msg model =
             ( { model | optionsOrder = ls }, Cmd.none )
 
 
-type alias Trial =
-    { uid : String
-    , question : String
-    , target : String
-    , translation2 : String
-    , distractor1 : String
-    , distractor2 : String
-    , distractor3 : String
-    , word : String
-    , isTraining : Bool
-    }
+
+-- HTTP
 
 
-defaultTrial : Trial
-defaultTrial =
-    { uid = ""
-    , question = "String"
-    , target = "String"
-    , translation2 = "String"
-    , distractor1 = ""
-    , distractor2 = ""
-    , distractor3 = ""
-    , word = "String"
-    , isTraining = False
-    }
+getRecords =
+    Http.task
+        { method = "GET"
+        , headers = []
+        , url =
+            Data.buildQuery
+                { app = Data.apps.spacing
+                , base = "input"
+                , view_ = "Presentation"
+                }
+        , body = Http.emptyBody
+        , resolver = Http.stringResolver <| Data.handleJsonResponse <| decodeTrials
+        , timeout = Just 5000
+        }
 
 
-type alias State =
-    { uid : String
-    , userAnswer : String
-    }
-
-
-initState : State
-initState =
-    { uid = ""
-    , userAnswer = ""
-    }
+getTrialsFromServer : (Result Http.Error (List Trial) -> msg) -> Cmd msg
+getTrialsFromServer msgHandler =
+    Data.getTrialsFromServer_ "input" "Translation" msgHandler decodeTrials
 
 
 decodeTrials : Decoder (List Trial)
@@ -141,10 +251,6 @@ decodeTrials =
     Data.decodeRecords decoder
 
 
-type alias SummarizedTrial =
-    { trialuid : String, userUid : String, attempt : String }
-
-
 encodeHistory : SummarizedTrial -> Encode.Value
 encodeHistory trial =
     Encode.object [ ( "userUid", Encode.string trial.userUid ), ( "trialUid", Encode.string trial.trialuid ), ( "attempt", Encode.string trial.attempt ) ]
@@ -154,107 +260,9 @@ payload history =
     Encode.list encodeHistory history
 
 
-renderTask task trial data history allTrials =
-    div [ class "w-full" ]
-        [ if List.isEmpty data.trainingTrials then
-            View.tooltip data.infos.instructions_short
 
-          else
-            div [] []
-        , if List.length data.trainingTrials > 0 then
-            div [] []
-
-          else
-            Progressbar.progressBar history allTrials
-        , View.fromMarkdown trial.question
-        , div
-            [ class "w-full pt-8", disabled data.feedback ]
-          <|
-            View.shuffledOptions
-                data.state
-                data.feedback
-                UserClickedRadioButton
-                trial
-                task.optionsOrder
-        , View.genericSingleChoiceFeedback
-            { isVisible = data.feedback
-            , userAnswer = data.state.userAnswer
-            , target = trial.target
-            , feedback_Correct = ( data.infos.feedback_correct, [ trial.target ] )
-            , feedback_Incorrect = ( data.infos.feedback_incorrect, [ trial.target ] )
-            , button = View.navigationButton UserClickedToggleFeedback UserClickedNextTrial data.feedback data.state.userAnswer
-            }
-        ]
-
-
-view :
-    { task : Logic.Task Trial State
-    , optionsOrder : List comparable
-    }
-    -> Html Msg
-view task =
-    case task.task of
-        Logic.Running Logic.Training data ->
-            case data.current of
-                Just trial ->
-                    div [ class "flex flex-col items-center" ]
-                        [ View.trainingWheelsGeneric (List.length data.history) data.infos.trainingWheel [ trial.target ]
-                        , renderTask task trial data data.history data.trainingTrials
-                        ]
-
-                Nothing ->
-                    View.introToMain UserClickedStartMain
-
-        Logic.Running Logic.Main data ->
-            case data.current of
-                Just trial ->
-                    div [ class "flex flex-col items-center" ]
-                        [ renderTask task trial data data.history data.mainTrials
-                        ]
-
-                Nothing ->
-                    View.end data.infos.end UserClickedSaveData "spelling"
-
-        Logic.Loading ->
-            View.loading
-
-        Logic.NotStarted ->
-            div [] [ text "The experiment is not started yet" ]
-
-        Logic.Err reason ->
-            div [] [ text reason ]
-
-        Logic.Running Logic.Instructions data ->
-            div [] [ View.instructions data.infos UserClickedStartTraining ]
+-- INTERNALS
 
 
 taskId =
     "recf5HANE632FLKbc"
-
-
-getRecords =
-    Http.task
-        { method = "GET"
-        , headers = []
-        , url =
-            Data.buildQuery
-                { app = Data.apps.spacing
-                , base = "input"
-                , view_ = "Presentation"
-                }
-        , body = Http.emptyBody
-        , resolver = Http.stringResolver <| Data.handleJsonResponse <| decodeTrials
-        , timeout = Just 5000
-        }
-
-
-start : List ExperimentInfo.Task -> List Trial -> Logic.Task Trial State
-start info trials =
-    let
-        relatedInfos =
-            Dict.get taskId (ExperimentInfo.toDict info) |> Result.fromMaybe ("I couldn't fetch the value associated with: " ++ taskId)
-    in
-    Logic.startIntro relatedInfos
-        (List.filter (\datum -> datum.isTraining) trials)
-        (List.filter (\datum -> not datum.isTraining) trials)
-        initState
